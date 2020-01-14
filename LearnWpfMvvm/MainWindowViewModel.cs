4325620b7c16e2e64601a54fa59c180222359b8b
Adam V Steele <adam@avsteele.com>
2020-01-14 16:16:22 -0500

    Example 5: Data Binding and TextBlock Formatting

--- LearnWpfMvvm/MainWindowViewModel.cs
+++ LearnWpfMvvm/MainWindowViewModel.cs
@@ -5,11 +5,35 @@ using System.Text;
 using System.Windows;
 using System.Windows.Input;
 
+using System.ComponentModel; // InotifyPropertyChanged
+using System.Runtime.CompilerServices;
+
 namespace LearnWpfMvvm
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : INotifyPropertyChanged
     {
         public ICommand CommandOne { get; }
+
+        #region INotifyPropertyChanged stuff
+        private int _executionCount = 0;
+        public int ExecutionCount 
+        { 
+            get => _executionCount;
+            private set
+            {
+                _executionCount = value;
+                RaisePropertyChanged();
+            }             
+        }
+
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
 
         public MainWindowViewModel()
         {
@@ -18,6 +42,7 @@ namespace LearnWpfMvvm
 
         public void ShowBox()
         {
+            ExecutionCount++;
             MessageBox.Show("Command Executed");
         }
     }
