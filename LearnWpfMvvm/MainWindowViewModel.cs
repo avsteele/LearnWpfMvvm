ec906b200f3fa5c1b83811b24d8edd8e687b678c
Adam V Steele <adam@avsteele.com>
2020-01-14 13:06:04 -0500

    Example 1: Most Basic ICommand and XAML usage

---
+++ LearnWpfMvvm/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Windows;
+using System.Windows.Input;
+
+namespace LearnWpfMvvm
+{
+    class MainWindowViewModel
+    {
+        public ICommand CommandOne { get; }
+
+        public MainWindowViewModel()
+        {
+            CommandOne = new SimpleCommand(ShowBox);
+        }
+
+        public void ShowBox()
+        {
+            MessageBox.Show("Command Executed");
+        }
+    }
+}
---
+++ LearnWpfMvvm/SimpleCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using System.Windows.Input; //For ICommand
+
+namespace LearnWpfMvvm
+{
+    public class SimpleCommand : ICommand
+    {
+        private Action _action;
+
+        public SimpleCommand(Action a)
+        {
+            _action = a;
+        }
+
+        public bool CanExecute(object o)
+        {
+            return true;
+        }
+
+        public void Execute(object o)
+        {
+            if (CanExecute(o))
+                _action();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
